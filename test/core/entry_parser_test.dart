ff898bdba6436a4fab33882e3e8dc0647216004b
Amos <amos@swiftllama.net>
2023-01-19 01:51:14 +0100

    parse lines instead of blocks

--- test/core/entry_parser_test.dart
+++ test/core/entry_parser_test.dart
@@ -9,137 +9,40 @@ final posting3 = Posting(account:'Liabilities:debit card', currency: r'EUR', amo
 
 
 void main() {
-  group('entry header', () {
-    test('basic parsing', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.entryHeader);
+  final definition = LedgerLineDefinition();
+  final parser = definition.build();
 
-      expect(parser.parse('2023/01/06 XYZ').value, [DateTime(2023, 01, 06), null, EntryState.uncleared, 'XYZ', []]);
+  group('entry header lines', () {
+    test('basic parsing', () {
+      expect(parser.parse('2023/01/06 XYZ').value, EntryLine(date:DateTime(2023, 01, 06), code:'', state:EntryState.uncleared, payee:'XYZ', note:''));
     });
 
     test('header with code', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.entryHeader);
-
-      expect(parser.parse('2023/02/04 (#100) ABC').value, [DateTime(2023, 02, 04), '#100', EntryState.uncleared, 'ABC', []]);
+      expect(parser.parse('2023/02/04 (#100) ABC').value, EntryLine(date:DateTime(2023, 02, 04), code:'#100', state:EntryState.uncleared, payee:'ABC', note:''));
     });
 
     test('header with state', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.entryHeader);
-
-      expect(parser.parse('2023/02/01 * DEF').value, [DateTime(2023, 02, 01), null, EntryState.cleared, 'DEF', []]);
+      expect(parser.parse('2023/02/01 * DEF').value, EntryLine(date:DateTime(2023, 02, 01), code:'', state:EntryState.cleared, payee:'DEF', note:''));
     });
 
     test('header with note', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.entryHeader);
-
-      expect(parser.parse('2023/02/01 * DEF GHI ; something or other').value, [DateTime(2023, 02, 01), null, EntryState.cleared, 'DEF GHI', ['something or other']]);
-    });
-
-    test('header with multiline notes', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.entryHeader);
-
-      expect(parser.parse('2023/02/01 * DEF GHI ; something or other\n ; on two lines').value, [DateTime(2023, 02, 01), null, EntryState.cleared, 'DEF GHI', ['something or other', 'on two lines']]);
+      expect(parser.parse('2023/02/01 * DEF GHI ; something or other').value, EntryLine(date: DateTime(2023, 02, 01), code:'', state:EntryState.cleared, payee:'DEF GHI', note:'something or other'));
     });
   });
 
-  group('posting header', () {
-    test('with amount', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.postingHeader);
+  group('note lines', () {
+    test('simple note line', () {
+      expect(parser.parse('  ; this is a note').value, NoteLine('this is a note'));
+    });
+  });
 
-      expect(parser.parse(r'    Expenses:books     $ 500').value, ['    ', 'Expenses:books', r'$', 500.0, []]);
+  group('posting line', () {
+    test('with amount', () {
+      expect(parser.parse(r'    Expenses:books     $ 500').value, PostingLine(account:'Expenses:books', currency:r'$', amount:500.0, note:''));
     });
 
     test('with note', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.postingHeader);
-
-      expect(parser.parse(r'    Expenses:books     $ -3.14159 ; this is a note').value, ['    ', 'Expenses:books', r'$', -3.14159, ['this is a note']]);
-    });
-  });
-
-  group('posting', () {
-    test('with amount', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.posting);
-
-      expect(parser.parse(r'    Expenses:books     $ 500').value, Posting(account: 'Expenses:books', currency: r'$', amount: 500.0));
-    });
-
-    test('with end-line note', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.posting);
-
-      expect(parser.parse(r'    Expenses:books     $ -3.14159 ; this is a note').value, Posting(account: 'Expenses:books', currency: r'$', amount: -3.14159, notes: 'this is a note'));
-    });
-
-    test('with multiline note', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.posting);
-
-      expect(parser.parse('    Expenses:books     \$ -3.14159 ; this is a note\n    ; on two lines').value, Posting(account: 'Expenses:books', currency: r'$', amount: -3.14159, notes: 'this is a note\non two lines'));
-    });
-  });
-
-  group('postings', () {
-    test('posting with amount', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.posting);
-
-      expect(parser.parse(r'    Expenses:books     $ 500').value, posting1);
-      expect(parser.parse(r'    Liabilities:credit card     EUR -500.3').value, posting2);
-    });
-
-    test('multiple postings', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.postings);
-      expect(parser.parse("""    Expenses:books     \$ 500
-    Liabilities:credit card     EUR -500.3
-""").value, [posting1, posting2]);
-    });
-  });
-
-  group('entries', () {
-
-    test('simple entry', () {
-      final definition = EntryDefinition();
-      final parser = definition.build(start: definition.entry);
-
-      final parsedEntry = parser.parse(
-          r"""2023/02/04 (#100) ABC
-    Expenses:books     $ 500
-    Liabilities:debit card     EUR -500
-""").value as Entry;
-
-      final expectedEntry =  Entry(date:DateTime(2023, 02, 04), code: '#100', payee: 'ABC', state: EntryState.uncleared, postings: [posting1, posting3]);
-      expect(parsedEntry.date, expectedEntry.date);
-      expect(parsedEntry.code, expectedEntry.code);
-      expect(parsedEntry.payee, expectedEntry.payee);
-      expect(parsedEntry.state, expectedEntry.state);
-      expect(parsedEntry.postings, expectedEntry.postings);
-    });
-  });
-
-
-
-  group('blarg', () {
-
-    test('simple entry', () {
-      final definition = BlargDefinition();
-      final parser = definition.build(start: definition.note);
-
-      expect(parser.parse("; this is a note").value, []);
-    });
-
-    test('multilinezz', () {
-      final definition = BlargDefinition();
-      final parser = definition.build(start: definition.note);
-
-      expect(parser.parse("; this is a note \n ; blarfds").value, []);
+      expect(parser.parse(r'    Expenses:books     $ -3.14159 ; this is a note').value,PostingLine(account:'Expenses:books', currency:r'$', amount:-3.14159, note:'this is a note'));
     });
   });
 }
