729573df8775da1094eba4183bdf06d5cec40c09
Amos <amos@example.com>
2023-01-21 23:34:25 +0100

    ledger file loader

--- lib/src/cli_wrapper/ledger_cli_wrapper.dart
+++ lib/src/cli_wrapper/ledger_cli_wrapper.dart
@@ -2,21 +2,3 @@ import 'dart:async';
 
 import "../core/core.dart";
 import 'cli_wrapper.dart';
-
-/*
-class LedgerCliWrapper implements TransactionSource {
-  static const queryArgumentFormatter = QueryArgumentFormatter();
-  final CliWrapper ledgerBinary;
-  String ledgerFile;
-  LedgerCliWrapper({required this.ledgerBinary, required this.ledgerFile});
-
-  static LedgerCliWrapper forLedger({required String ledgerBinary, required String ledgerFile}) => LedgerCliWrapper(ledgerBinary: CliWrapper(executablePath: ledgerBinary), ledgerFile: ledgerFile);
-
-  @override
-  Future<Stream<Transaction>> query(Query query) async {
-    final args = ['-f', ledgerFile, 'csv'];
-    args.addAll(queryArgumentFormatter.asList(query));
-    final csvData = await ledgerBinary.execute(args);
-    return Stream.value(csvData).transform(CsvToListConverter(eol: "\n", shouldParseNumbers: true, allowInvalid: false)).transform(ListToTransactionConverter());
-  }
-}*/
--- lib/src/parser/parser_lib.dart
+++ lib/src/parser/parser_lib.dart
@@ -2,9 +2,10 @@ library parser;
 
 export 'lines/ledger_line_parser.dart';
 export 'lines/ledger_line.dart';
-export 'line_to_entry_converter.dart';
+export 'ledger_file_loader.dart';
 export 'lines/string_to_line_transformer.dart';
 export 'edits/line_to_edit_transformer.dart';
 export 'edits/edits.dart';
 export 'edits/ledger_edit_receiver.dart';
 export 'edits/ledger_edit_applier.dart';
+export 'ledger_file_loader.dart';
