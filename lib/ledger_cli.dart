3c2e3299e3110dca385829bd4126a57e1549ea2c
Amos <amos@example.com>
2023-01-22 00:48:43 +0100

    clean up readme

--- lib/ledger_cli.dart
+++ lib/ledger_cli.dart
@@ -1,5 +1,4 @@
 library ledger_cli;
 
 export 'src/core/core.dart';
-export 'src/cli_wrapper/cli_wrapper_lib.dart';
 export 'src/parser/parser_lib.dart';
--- lib/src/core/core.dart
+++ lib/src/core/core.dart
@@ -3,7 +3,6 @@ library core;
 export "query.dart";
 export "entry.dart";
 export "posting.dart";
-export "transaction_source.dart";
-export "entry_string_codec.dart";
 export 'account.dart';
 export 'ledger.dart';
+export 'formatters.dart';
