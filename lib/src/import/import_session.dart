443ec65a786bc2571e17f56252c35eb8edba15be
Amos <amos@swiftllama.net>
2023-03-20 14:15:32 +0100

    basic import session tests

--- lib/src/import/import_session.dart
+++ lib/src/import/import_session.dart
@@ -6,15 +6,10 @@ import 'import_account.dart';
 class ImportSession {
   final List<PendingImportedEntry> pendingEntries = [];
 
-  Future<void> _loadCsvLines(Stream<CsvLine> csvLines, {required ImportAccount importAccount}) async {
+  Future<void> loadCsvLines(Stream<CsvLine> csvLines, {required ImportAccount importAccount}) async {
     await for (final csvLine in csvLines) {
       pendingEntries.add(PendingImportedEntry(csvLine: csvLine, importAccount: importAccount));
     }
     pendingEntries.sort((line1, line2) => line1.csvLine.date.compareTo(line2.csvLine.date));
-  }
-
-  Future<void> loadCsvData(Stream<List<dynamic>> csvDataStream, {required ImportAccount importAccount}) async {
-    final csvLineStream = csvDataStream.transform(ListToCsvLineTransformer(csvFormat: importAccount.csvFormat));
-    await _loadCsvLines(csvLineStream, importAccount: importAccount);
   }
 }
